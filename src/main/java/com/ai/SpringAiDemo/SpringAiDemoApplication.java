c29a2704045566c29ac53917ccbc282d87ca19e8
ShashithaU <udeshshashitha@gmail.com>
2025-06-03 23:33:14 +0530

    implemented basic chat endpoint

--- src/main/java/com/ai/SpringAiDemo/SpringAiDemoApplication.java
+++ src/main/java/com/ai/SpringAiDemo/SpringAiDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiDemoApplication {
+public class  SpringAiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiDemoApplication.class, args);
